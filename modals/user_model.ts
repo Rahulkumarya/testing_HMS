26397dd783e60277488e1b011c2fa496b974448d
rahulkumaryadav <127029912+Rahulkumarya@users.noreply.github.com>
2025-05-01 19:20:24 +0530

    Update user_model.ts

--- modals/user_model.ts
+++ modals/user_model.ts
@@ -56,7 +56,7 @@ const userSchema: Schema<IUser> = new mongoose.Schema(
     role: {
       type: String,
       default: "user",
-      enum:["patient","doctor","diagnostic","hospital","medicine","resort"]
+      enum:["patient","doctor","diagnostic","hospital","medicine","resort","user"]
     },
     isVerified: {
       type: Boolean,
